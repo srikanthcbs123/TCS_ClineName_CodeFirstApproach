029f3b1d56aa83c5188569c83bea8c846d294cf4
Dell <Dell@DESKTOP-AAO14OC>
2024-11-12 06:57:55 +0530

    Userstory_123456:Department crudopertaions implemented

---
+++ TCS_ClineName_CodeFirstApproach/Interfaces/IDepartmentService.cs
@@ -0,0 +1,12 @@
+﻿namespace TCS_ClineName_CodeFirstApproach
+{
+    public interface IDepartmentService
+    {
+
+        Task<List<DepartementDto>> GetDepartments();
+        Task<DepartementDto> GetDepartmentById(int deptid);
+        Task<int> AddDepartments(DepartementDto deptdetail);
+        Task<bool> DeleteDepartmentById(int deptid);
+        Task<bool> UpdateDepartment(DepartementDto deptdetail);
+    }
+}
